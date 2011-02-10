975b0b9479d73c14db3f622fd79af5fa4624a42a
Adam Crabtree <dude@noderiety.com>
2011-02-10 13:33:28 -0600

    Fixed expectation in inheritance.js

--- examples/inheritance/inheritance.js
+++ examples/inheritance/inheritance.js
@@ -3,11 +3,11 @@ var Person = Class.extend(function(pvtPerson) {
 	
 	return {
 		init: function(isDancing) {
-//			console.log("pvtPerson(this).invisible: \r\n >> ", pvtPerson(this).invisible); // => undefined
-//			console.log("pvtPerson(this).hasSword: \r\n >> ", pvtPerson(this).hasSword); // => undefined
+			console.log("pvtPerson(this).invisible: \r\n >> ", pvtPerson(this).invisible); // => undefined
+			console.log("pvtPerson(this).hasSword: \r\n >> ", pvtPerson(this).hasSword); // => undefined
 			
 			pvtPerson(this).dancing = isDancing;
-//			console.log("pvtPerson(this).dancing: \r\n >> ", pvtPerson(this).dancing); // => undefined
+			console.log("pvtPerson(this).dancing: \r\n >> ", pvtPerson(this).dancing); // => undefined
 		},
 		dance: function() {
 			return pvtPerson(this).dancing;
@@ -25,7 +25,7 @@ var Ninja = Person.extend(function(pvtNinja) {
 			this._super( false );
 			pvtNinja(this).hasSword = hasSword || true;
 
-			console.log("pvtNinja(this).dancing: \r\n >> ", pvtNinja(this).dancing); // => undefined
+			console.log("pvtNinja(this).dancing: \r\n >> ", pvtNinja(this).dancing); // => false
 		},
 		dance: function(){
 			// Call the inherited version of dance()
@@ -39,14 +39,14 @@ var Ninja = Person.extend(function(pvtNinja) {
 
 
 var p = new Person(true);
-//console.log("p.dance(): \r\n >> ",p.dance()); // => true
+console.log("p.dance(): \r\n >> ",p.dance()); // => true
 
 var n = new Ninja();
-//console.log("n.dance(): \r\n >> ",n.dance()); // => false
-//console.log("n.swingSword(): \r\n >> ",n.swingSword()); // => true
-//
-//// Should all be true
-//console.log("p instanceof Person && p instanceof Class && " +
-//	"n instanceof Ninja && n instanceof Person && n instanceof Class: \r\n >> ",
-//	p instanceof Person && p instanceof Class &&
-//	n instanceof Ninja && n instanceof Person && n instanceof Class);
+console.log("n.dance(): \r\n >> ",n.dance()); // => false
+console.log("n.swingSword(): \r\n >> ",n.swingSword()); // => true
+
+// Should all be true
+console.log("p instanceof Person && p instanceof Class && " +
+	"n instanceof Ninja && n instanceof Person && n instanceof Class: \r\n >> ",
+	p instanceof Person && p instanceof Class &&
+	n instanceof Ninja && n instanceof Person && n instanceof Class);
